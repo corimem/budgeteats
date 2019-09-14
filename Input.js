1cc288983bc47e0ac9141ee57435728a0ebd18c9
lbainbri <54671844+lbainbri@users.noreply.github.com>
2019-09-14 15:39:08 -0400

    Update Input.js

--- Input.js
+++ Input.js
@@ -1,9 +1,30 @@
 //vars
 var budgetPrice;
 var time;
+var foods = [];
+var foodsLength = 12;
+var allergies = [];
+var allergiesLength = 2;
 
 //input function
 function input(){
     budgetPrice = document.getElementById("budget").value;
     time = document.getElementById("time").value;
+
+    //set values in array of foods
+    for(var i = 0; i < foodsLength; i ++){
+        foods[i] = document.getElementById(i + "F").checked;
+    }
+
+    //set values in array of allergies
+    /*for(var i = 0; i < allergiesLength; i ++){
+        allergies[i] = document.getElementById(i + "A").checked;
+    }*/
+}
+
+function dropdown(id) {
+    if (document.getElementById(id).classList.contains("visible"))
+        document.getElementById(id).classList.remove("visible");
+    else
+        document.getElementById(id).classList.add("visible");
 }
