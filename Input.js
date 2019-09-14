90789ef26f52ad8d48028e9692967c571c4a3322
lbainbri <54671844+lbainbri@users.noreply.github.com>
2019-09-14 14:32:48 -0400

    Update Input.js

--- Input.js
+++ Input.js
@@ -1,17 +1,9 @@
 //vars
 var budgetPrice;
 var time;
-var foods;
-var appliances;
-var allergies;
-var diet;
-var something;
-var something2;
 
 //input function
 function input(){
     budgetPrice = document.getElementById("budget").value;
     time = document.getElementById("time").value;
-    foods = document.getElementById("foods").value;
-    appliances =
 }
